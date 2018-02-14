33aee7ef9d0177febb4dc90306c5a6a7acba5faf
boboww <32184879+boboww@users.noreply.github.com>
2018-02-14 16:12:49 -0500

    test branch

--- Lab 3 - BCD Counter and FPGA Download/Lab3/binary_bcd.v
+++ Lab 3 - BCD Counter and FPGA Download/Lab3/binary_bcd.v
@@ -7,7 +7,7 @@ module binary_bcd_2(bin_in, digit_1, digit_2);
 	// Binary-to-BCD logic goes here
 	integer i;
 	always @(bin_in) begin
-		for(i = 6; i >= 0; i = i-1) begin
+		for(i = 6; i >= 0; i = i - 1) begin
 			if (digit_1 >= 5)
 				digit_1 = digit_1 + 3;
 			if (digit_2 >= 5)
